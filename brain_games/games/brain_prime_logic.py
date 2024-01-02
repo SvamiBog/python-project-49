b155ec1f79d2056798560cb7c1b2617932972ac4
Maksim <rubanezmaxim@gmail.com>
2024-01-02 13:13:31 +0100

    Fixing files to comply with PEP8 standards.

--- brain_games/games/brain_prime_logic.py
+++ brain_games/games/brain_prime_logic.py
@@ -4,7 +4,7 @@ import random
 
 
 INSTRUCTION_MESSAGE = "Answer \"yes\" if given number is prime. "\
-                            "Otherwise answer \"no\"."
+                      "Otherwise answer \"no\"."
 
 
 def generate_question():
