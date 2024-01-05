7a8f056ab397359903997acdf652473fd390ace3
Maksim <rubanezmaxim@gmail.com>
2024-01-05 22:23:18 +0100

    fixed file brain_gcd_logic.py

--- brain_games/games/brain_gcd_logic.py
+++ brain_games/games/brain_gcd_logic.py
@@ -10,10 +10,10 @@ def generate_question():
     num1 = random.randint(1, 50)
     num2 = random.randint(1, 50)
     question = f"{num1} {num2}"
-    return question, get_correct_answer(num1, num2)
+    return question, str(get_correct_answer(num1, num2))
 
 
 def get_correct_answer(num1, num2):
     while num2:
         num1, num2 = num2, num1 % num2
-    return str(num1)
+    return num1
